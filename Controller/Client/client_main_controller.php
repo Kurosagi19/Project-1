2d1e599a2510355f86905b281ebb5cc824a51674
Kurosagi19 <thanhlanqwerty@gmail.com>
2023-03-24 16:44:54 +0700

    something changed

--- Controller/Client/client_main_controller.php
+++ Controller/Client/client_main_controller.php
@@ -2,4 +2,14 @@
 $redirect = $_GET['redirect'] ?? '';
 require_once('Model/Client/index_model.php');
 require_once('Views/Client/index.php');
+
+if($redirect == '') {
+    
+} else {
+    switch($redirect) {
+        case 'product':
+            require_once('Model/Client/client_model.php');
+            require_once('Views/Client/product.php');
+        }
+}
 ?>
